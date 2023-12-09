24f0d0f6720cc9bc6d54fa264c0312903d5e22ff
Mauricio Calderon Garro <mauricioantonicg@uned.cr>
2023-12-09 00:00:16 -0600

    Se crearon los metodos para consultar la informacion a la api

--- CP_Control_Aviones/Controllers/HomeController.cs
+++ CP_Control_Aviones/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -17,8 +19,57 @@ namespace CP_Control_Aviones.Controllers
 
       public async Task<string> probarConexionConApi()
       {
-         var httpClient = new HttpClient();
-         var resultado = await httpClient.GetStringAsync("https://localhost:44303/Home/pruebaConexion");
+
+         //Consultar api con get
+
+         //String con la url de la api
+         //string urlGet = "https://localhost:44303/Home/pruebaConexion";
+
+         ////Variable de http para la conexion 
+         //var clienteHttp = new HttpClient();
+
+         ////Limpiar cabecera de la peticion ya que no se requiere token de autorizacion 
+         //clienteHttp.DefaultRequestHeaders.Clear();
+
+         ////Variable que almacena la respuesta de la api 
+         //var respuesta = clienteHttp.GetAsync(urlGet).Result;
+
+         ////Variable para leer la respuesta 
+         //var leerRespuesta = respuesta.Content.ReadAsStringAsync().Result;
+
+         ////Convertir la respuesta en Json 
+         //dynamic jsonRespuesta = JObject.Parse(leerRespuesta);
+
+
+
+         //Consultar api con post
+
+         //String con la url de la api
+         string urlPost = "https://localhost:44303/Home/pruebaConexionPost";
+
+         //Variable de http para la conexion 
+         var clienteHttp = new HttpClient();
+
+         //Limpiar cabecera de la peticion ya que no se requiere token de autorizacion 
+         clienteHttp.DefaultRequestHeaders.Clear();
+
+         //Parametros que llevara la peticion 
+         string parametros = "{ 'prueba': '1', 'prueba2': '0' }";
+
+         //Convertir los parametros a Json
+         dynamic jsonParametros = JObject.Parse(parametros);
+
+         //Agregar el contenido o parametros a la peticion 
+         var contenidoHttp = new StringContent(Convert.ToString(jsonParametros), Encoding.UTF8, "application/json");
+
+         //Variable que almacena la respuesta de la api 
+         var respuesta = clienteHttp.PostAsync(urlPost, contenidoHttp).Result;  
+
+         //Variable para leer la respuesta 
+         var leerRespuesta = respuesta.Content.ReadAsStringAsync().Result;
+
+         //Convertir la respuesta en Json 
+         dynamic jsonRespuesta = JObject.Parse(leerRespuesta);
 
          return "";
       }
